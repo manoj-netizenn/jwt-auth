eee126ae33abf9464c9bc97c350b1928bc2e691e
manoj-netizenn <mallappanavarmanoj@gmail.com>
2025-03-06 23:17:19 +0530

    fixing deployment issue

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-const { MongoClient } = require("mongodb");
 const cookieParser = require("cookie-parser");
 dotenv.config();
 
